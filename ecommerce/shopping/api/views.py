f4a00bd55735cbbfc28f126ae7586972fadfdd13
Josmanuel Sandrea <josmanueljsg@gmail.com>
2023-09-13 23:52:41 -0500

    Created the shopping_list API, now you can create shopping lists (without items yet)

--- ecommerce/shopping/api/views.py
+++ ecommerce/shopping/api/views.py
@@ -1,17 +1,38 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from shopping.models import Shopping_List, Shopping_List_Item
 from shopping.api.serializers import ShoppingListSerializer
-from rest_framework.permissions import IsAuthenticated
+from users.models import User
+
 
 class ShoppingListAPIView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
-        shopping_list = Shopping_List.objects.filter(user=request.user.id)[0]
-        print(shopping_list)
-        serializer = ShoppingListSerializer(data=shopping_list)
-        if serializer.is_valid():
+        # Get the shopping list of an user
+        shopping_list = Shopping_List.objects.filter(user=request.user.id)
+
+        # If there is not existing shopping list for the user
+        if (len(shopping_list) > 0):
+            # Serialize the data
+            serializer = ShoppingListSerializer(shopping_list, many=True)
             return Response(serializer.data)
         else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': 'User does not have a shopping list'})
+    
+    def post(self, request):
+        # Check if there is an existing shopping list for the user
+        shopping_list = Shopping_List.objects.filter(user=request.user.id)
+
+        # If there is one (at least one) return the data of the current list
+        if (len(shopping_list) > 0):
+            serializer = ShoppingListSerializer(shopping_list, many=True)
+            return Response(serializer.data)
+        # In case there is not existing shopping_list, create one
+        else:
+            current_authenticated_user = User.objects.get(id=request.user.id)
+            created_list = Shopping_List.objects.create(user=current_authenticated_user)
+            serializer = ShoppingListSerializer(created_list)
+            return Response(serializer.data)
+        
