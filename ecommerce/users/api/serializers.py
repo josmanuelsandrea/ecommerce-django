e13d2d9e1ab1d7c9b62ec2996454dfc2919353bf
Josmanuel Sandrea <josmanueljsg@gmail.com>
2023-12-03 17:20:04 -0500

    Modified data fields to login and register by modifying the serializers and the user model. Now you log in with email, not with username

--- ecommerce/users/api/serializers.py
+++ ecommerce/users/api/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from users.models import User
+from uuid import uuid4
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'email', 'password']
     
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
+        username = validated_data.pop('username', uuid4())
+        instance = self.Meta.model(username=username, **validated_data)
         if password is not None:
             instance.set_password(password)
         instance.save()
@@ -17,9 +19,13 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'first_name', 'last_name']
+        fields = ['id', 'email', 'password', 'first_name', 'last_name']
 
 class UserInfoNeeded(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'email']
+
+class CustomTokenObtainPairSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
